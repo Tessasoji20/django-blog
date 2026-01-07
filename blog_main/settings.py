e3066829eb33d2a8bcb61d86a61a6f119a80eae8
Tessasoji20 <tessasoji@gmail.com>
2026-01-07 07:55:35 +0530

    blogs app and blog model

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -36,7 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles','blogs',
 ]
 import os
 MIDDLEWARE = [
@@ -116,3 +116,5 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
